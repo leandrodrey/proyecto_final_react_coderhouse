3cec1e79dd23fd63b245fd135b70c58bc9bc7388
Leandro Rey <leandrodrey@gmail.com>
2023-06-10 16:48:08 -0300

    firebase.config.jsx updated

--- src/services/firebase.config.jsx
+++ src/services/firebase.config.jsx
@@ -2,19 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCbd_WlTnTfQOY_n3ibV4Ezjf4oRe2ythA",
-    authDomain: "proyectofinal-43185.firebaseapp.com",
-    projectId: "proyectofinal-43185",
-    storageBucket: "proyectofinal-43185.appspot.com",
-    messagingSenderId: "505510427698",
-    appId: "1:505510427698:web:95b8563a644ce9ee66902b",
-    measurementId: "G-X7S8NKP81S"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase

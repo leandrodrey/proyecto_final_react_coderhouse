be9af3faba602ff124a05acd012090d5399d5e13
leandrodrey <leandrodrey@gmail.com>
2023-06-10 20:24:28 -0300

    Some styles fixed for mobile

--- src/components/NavBar/NavBarTitle.jsx
+++ src/components/NavBar/NavBarTitle.jsx
@@ -2,31 +2,14 @@ import React from 'react'
 import Typography from "@mui/material/Typography";
 import {NavLink} from "react-router-dom";
 
-const NavBarTitle = (props) => {
-
-    const {title,logoImage} = props;
+const NavBarTitle = ({title,logoImage}) => {
 
     return (
         <React.Fragment>
             <NavLink to={'/'}>
                 <img className="navbar__logo" alt={title} src={logoImage} />
             </NavLink>
-            <Typography
-                variant="h4"
-                noWrap
-                component="a"
-                href="/"
-                sx={{
-                    mr: 2,
-                    display: { xs: 'none', md: 'flex' },
-                    fontFamily: 'monospace',
-                    fontWeight: 800,
-                    letterSpacing: '0',
-                    color: 'inherit',
-                    textDecoration: 'none',
-                    size: '3rem',
-                }}
-            >
+            <Typography className="site_title" variant="h1">
                 {title}
             </Typography>
         </React.Fragment>

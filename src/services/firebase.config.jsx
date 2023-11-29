d7971ec7e9bc8168bef4645c3a254bb8021e31f0
Leandro Rey <leandrodrey@gmail.com>
2023-11-29 18:25:02 -0300

    Update firebase.config.jsx to test prupouses

--- src/services/firebase.config.jsx
+++ src/services/firebase.config.jsx
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
-const firebaseConfig = {
+/* const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
     projectId: process.env.REACT_APP_projectId,
@@ -10,6 +10,16 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_messagingSenderId,
     appId: process.env.REACT_APP_appId,
     measurementId: process.env.REACT_APP_measurementId,
+};*/
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCbd_WlTnTfQOY_n3ibV4Ezjf4oRe2ythA",
+    authDomain: "proyectofinal-43185.firebaseapp.com",
+    projectId: "proyectofinal-43185",
+    storageBucket: "proyectofinal-43185.appspot.com",
+    messagingSenderId: "505510427698",
+    appId: "1:505510427698:web:95b8563a644ce9ee66902b",
+    measurementId: "G-X7S8NKP81S"
 };
 
 // Initialize Firebase
